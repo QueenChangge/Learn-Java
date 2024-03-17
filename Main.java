80c2fe5561a678dd3e0e02e21a4e38fba1a234bc
QueenChangge <agungcitra2717@gmail.com>
2024-03-17 18:51:04 +0800

    swap 2 variables

--- Main.java
+++ Main.java
@@ -2,15 +2,27 @@ public class Main
 {
   public static void main(String [] args) 
   { 
-    //VARIABLE
-    int x = 123;
-    double z = 18.9;
-    float a = 13.2f;
-    boolean c = false;
-    //false and true
-    char d = '%';
-    String y = "you wanna try something?";
-    System.out.println("My number is : " + x + z + a + c + d + y);
-    
+    // //VARIABLE
+    // int x = 123;
+    // double z = 18.9;
+    // float a = 13.2f;
+    // boolean c = false;
+    // //false and true
+    // char d = '%';
+    // String y = "you wanna try something?";
+    // System.out.println("My number is : " + x + z + a + c + d + y);
+
+
+    // SWAP TWO VARIABLES
+    String x = "water";
+    String y = "Kool-Aid";
+    String temp = null;
+
+    temp = x;
+    x = y;
+    y = temp;
+
+    System.out.println( "x: " + x);
+    System.out.println( "y: " + y);
   } 
 } 
